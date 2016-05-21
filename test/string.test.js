6d40e68541612de89222846a19b1d45d918bc1dc
jaywcjlove <398188662@qq.com>
2016-05-22 02:33:18 +0800

    Add test cases.

--- test/string.test.js
+++ test/string.test.js
@@ -1,3 +1,30 @@
 var toxic = require('../toxic')
-console.log( "underline string".underline.red );
-console.log( "underline string".yellow.underline );
+
+// console.log( "underline string".underline.red );
+// console.log( "underline string".yellow.underline );
+
+
+var styles = [
+  'bold', 'faint', 'italic', 'underline', 'blink', 'overline',
+  'inverse', 'conceal', 'strike'
+];
+var colors = [
+  'black', 'red', 'green', 'yellow', 'blue',
+  'magenta', 'cyan', 'white', 'grey', 'gray'
+];
+
+
+console.log("")
+console.log("styles:")
+
+styles.forEach(function(key) {
+    console.log("  "+key[key].red)
+    // console.log(key[key].red)
+});
+
+console.log("")
+console.log("colors:")
+
+colors.forEach(function(key) {
+  console.log("  "+key[key])
+});
